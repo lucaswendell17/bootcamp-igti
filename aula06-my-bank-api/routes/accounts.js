6ef30284fbccaca136637ec9d848f7e7868f9fa2
Lucas Wendel <lucas.wmartins@hotmail.com>
2020-08-06 12:17:52 -0300

    Add account methods

--- aula06-my-bank-api/routes/accounts.js
+++ aula06-my-bank-api/routes/accounts.js
@@ -15,9 +15,77 @@ router.post('/', async (req, res) => {
 
     data.accounts.push(account);
 
-    await writeFile(global.filename, JSON.stringify(data), null, 2);
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.filename));
+    delete data.nextId;
+    res.send(data);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.filename));
+    const account = data.accounts.find(
+      (account) => account.id === parseInt(req.params.id)
+    );
+    if (!account) res.status(400).send({ error: 'Account not found.' });
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.filename));
+    data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
+    res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile(global.filename));
+
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    data.accounts[index] = account;
+
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.patch('/updateBalance', async (req, res) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile(global.filename));
+
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    data.accounts[index].balance = account.balance;
+
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
+    res.send(data.accounts[index]);
   } catch (err) {
     res.status(400).send({ error: err.message });
   }
